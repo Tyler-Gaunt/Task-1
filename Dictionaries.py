85c55cc5d67345172aeaeb26e0d35e596f7b3e94
Tyler <TGaunt@student.saintaug.nsw.edu.au>
2024-02-28 14:19:41 +1100

    first commit

---
+++ Dictionaries.py
@@ -0,0 +1,44 @@
+car = {
+    "Category_name": "Cars",
+    "words": ["Subaru", "Ford", "Toyota", "Audi"]
+}
+IIID_Shapes = {
+    "Category_name": "3D shapes",
+    "words": ["Cone", "Cube", "Pyramid", "Sphere"]
+}
+complain = {
+    "Category_name": "Synonyms for complain",
+    "words": ["Carp", "Gripe", "Grouse", "Moan"]
+}
+wings = {
+    "Category_name": "Things with wings",
+    "words": ["Airplane", "Angel", "Bird", "Pegasus"]
+}
+Tom = {
+    "Category_name": "Toms",
+    "words": ["Cruise", "Holland", "Petty", "Waits"]
+}
+animal_noses = {
+    "Category_name": "Animal noses",
+    "words": ["Snout", "Beak", "Muzzle", "Trunk"]
+}
+common_colours = {
+    "Category_name": "Common colours",
+    "words": ["Blue", "Green", "Yellow", "Red"]
+}
+uncommon_colours = {
+    "Category_name": "Uncommon Colours",
+    "words": ["Ultramarine", "Celadon", "Aureolin", "Vermilion"]
+}
+animal_groups = {
+    "Category_name": "Animal Group Names",
+    "words": ["School", "Flock", "Pod", "Pack"]
+}
+island_countries = {
+    "Category_name": "Countries that are Islands",
+    "words": ["Malta", "Cuba", "Palau", "Cyprus"]
+}
+celestial_objects = {
+    "Category_name": "Celestial Objects",
+    "words": ["Planet", "Comet", "Moon", "Asteroid"]
+}
---
+++ from Dictionaries import car.py
@@ -0,0 +1,3 @@
+from Dictionaries import complain
+
+print(complain)
